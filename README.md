d83412566abcb3327a0fee8b86734dede8f4bcc3
Laurent Declercq <l.declercq@nuxwin.com>
2016-05-21 15:48:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# i-MSCP MonstaFTP Package 
+
+Repository which provides the i-MSCP MonstaFTP composer package. This package is automatically fetched and installed
+by the i-MSCP installer when needed.
+
+Upstream release: 1.8.x Serie
---
+++ iMSCP/config.php
@@ -0,0 +1,22 @@
+<?php
+
+# Open README file for descriptions and help.
+
+$ftpHost = "localhost";
+$ftpPort = "21";
+$ftpMode = "1";
+$ftpSSL  = "0";
+$ftpDir  = "";
+$serverTmp = "{TMP_PATH}";
+$editableExts = "";
+$sessionName = "";
+$dateFormatUsa = 0;
+$lockOutTime = 5;
+$versionCheck = 1;
+$showAdvOption = 1;
+$showLockSess = 1;
+$showHostInfo = 1;
+$showAddons = 1;
+$showDotFiles = 1;
+
+?>
